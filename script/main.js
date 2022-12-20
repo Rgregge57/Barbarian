a426e52072f43d2008fcb2f8d53eb0de852798c1
Rgregge57 <richardgregge7@outlook.com>
2022-12-20 15:06:23 +0000

    intial commit

---
+++ script/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.height = "50%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
